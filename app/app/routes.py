bd3cc60391e7abd221ff0d9a92deffe11dc4fc5d
sharedphysics <sharedphysics@gmail.com>
2025-04-04 13:20:48 -0600

    Registration and Login updates

--- app/app/routes.py
+++ app/app/routes.py
@@ -1,8 +1,12 @@
-from app import app
+from app import app, db, login
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+import sqlalchemy as sa
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
+from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.exceptions import abort
-from app.forms import LoginForm
+from urllib.parse import urlsplit
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -20,6 +24,7 @@ def get_post(post_id):
 
 @app.route('/')
 @app.route('/index')
+@login_required # If you want to toggle someone forced to log in to see roadmap, you can use this. 
 def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
@@ -80,12 +85,39 @@ def delete(id):
     flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+    
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+    
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
